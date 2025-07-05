c1f3910db7eda430c103551d3b05630ddac56e1d
Donats <chirkindonat@gmail.com>
2025-07-05 20:58:38 +0300

    New structure

---
+++ __init__.py
@@ -0,0 +1,11 @@
+﻿import os
+from flask import Flask
+from app.server import setup_routes
+from app.bot import run_bot
+
+app = Flask(__name__)
+setup_routes(app)     
+run_bot()             
+
+if __name__ == '__main__':
+    app.run()
--- passenger_wsgi.py
+++
@@ -1,8 +0,0 @@
-﻿# -*- coding: utf-8 -*-
-import sys
-import os
-
-sys.path.insert(0, os.path.dirname(__file__))
-sys.path.append('/home/givanu3m/.local/lib/python3.6/site-packages')  # путь до user-библиотек
-
-from app import app as application 
